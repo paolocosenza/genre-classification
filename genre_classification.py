8f666b5d79082179b65bd71a0e60180c0bb4bf2e
Paolo Cosenza <113355625+paolocosenza@users.noreply.github.com>
2023-02-05 08:11:08 +0100

    Update genre_classification.py

--- genre_classification.py
+++ genre_classification.py
@@ -1,7 +1,7 @@
 import librosa, librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
+import plotly as px
 import os
 
 from IPython.display import Image
