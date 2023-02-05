37f44dbb699c7eff8cae6b8c0d6ce8fb29d0d225
Paolo Cosenza <113355625+paolocosenza@users.noreply.github.com>
2023-02-05 08:31:55 +0100

    Update genre_classification.py

--- genre_classification.py
+++ genre_classification.py
@@ -126,7 +126,7 @@ if __name__=='__main__':
             # Converting the amplitude to decibels
             log_spectro = librosa.amplitude_to_db(spectrogram)
             # Plotting the short-time Fourier Transformation
-            fig=plt.figure(figsize=(4.32, 2.88))
+            spec=plt.figure(figsize=(4.32, 2.88))
             # Using librosa.display.specshow() to create our spectrogram
             librosa.display.specshow(log_spectro, sr=sr, hop_length=hop_length, cmap='magma')
             plt.savefig('spectrogram.png')
@@ -136,8 +136,7 @@ if __name__=='__main__':
 
             pred,pred_idx,probs = learn_inf.predict('spectrogram.png')
 
-            st.write("Song correctly downloaded! Here's the spectrogram I used to classify the song:")
-            st.pyplot(fig)
+            st.write('Looks like you were listening to a ' + pred + ' track! I can assess that with ' + str(round(float(probs[pred_idx])*100)) + '% probability.')
 
             df = pd.DataFrame(dict(
                 r=probs,
@@ -145,6 +144,8 @@ if __name__=='__main__':
             fig = plotly.express.line_polar(df, r='r', theta='theta', line_close=True)
             st.plotly_chart(fig, use_container_width=True)
 
-            st.write('Looks like you were listening to a ' + pred + ' track! I can assess that with ' + str(round(float(probs[pred_idx])*100)) + '% probability.')
+            st.write("Here's the spectrogram I used to classify the song:")
+            st.pyplot(spec)
+                     
             os.remove('predict.mp3')
             os.remove('spectrogram.png')
